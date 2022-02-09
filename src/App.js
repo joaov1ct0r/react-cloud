d900b1a7b9876737d7f0911e782627d7d523f11c
Joao Victor <jov9051@outlook.com>
2022-02-09 19:13:20 -0300

    elemento p criado dentro do header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
             <Nav></Nav>
             <header>
                 <h2>Cloud Computing</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris elit mi, fringilla ac congue sed, imperdiet sed enim.
+                    Quisque luctus eget ex ut commodo. Nullam non velit sodales.
+                </p>
             </header>
         </div>
     );
