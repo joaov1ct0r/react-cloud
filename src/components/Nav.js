e067e217ad7fdc8b3617f07dd2b04f7f112abe62
Joao Victor <jov9051@outlook.com>
2022-02-09 19:07:48 -0300

    codigo Nav movido para componente Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,31 @@
-function Nav() {}
+function Nav() {
+    return (
+        <nav>
+            <a href="http://localhost:3000">YOUR</a>
+
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+
+                <li>
+                    <a href="#about">About us</a>
+                </li>
+
+                <li>
+                    <a href="#work">Work</a>
+                </li>
+
+                <li>
+                    <a href="#info">Info</a>
+                </li>
+
+                <li>
+                    <a href="#start">Get Started</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
 
 export default Nav;
