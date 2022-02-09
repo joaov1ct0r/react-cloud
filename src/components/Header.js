c29e85b3b80364a8c8b992affaee7557c66e9f1d
Joao Victor <jov9051@outlook.com>
2022-02-09 19:16:43 -0300

    codigo header movido para componente Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,15 @@
-function Header() {}
+function Header() {
+    return (
+        <header>
+            <h2>Cloud Computing</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                elit mi, fringilla ac congue sed, imperdiet sed enim. Quisque
+                luctus eget ex ut commodo. Nullam non velit sodales.
+            </p>
+            <button type="submit">About us</button>
+        </header>
+    );
+}
 
 export default Header;
