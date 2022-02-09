9603d1e9ed9f23c93b311c5242123479fef99f7b
Joao Victor <jov9051@outlook.com>
2022-02-09 19:08:08 -0300

    codigo nav removido de app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <nav>
-                <a href="http://localhost:3000">YOUR</a>
-
-                <ul>
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-
-                    <li>
-                        <a href="#about">About us</a>
-                    </li>
-
-                    <li>
-                        <a href="#work">Work</a>
-                    </li>
-
-                    <li>
-                        <a href="#info">Info</a>
-                    </li>
-
-                    <li>
-                        <a href="#start">Get Started</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
