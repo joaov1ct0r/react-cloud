095b3b6f4f50cf840c9b7323052b37b46d818103
Joao Victor <jov9051@outlook.com>
2022-02-10 20:00:22 -0300

    codigos p e button movidos para div about

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,14 @@ function Header() {
                 />
             </div>
 
-            <div className="about"></div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                elit mi, fringilla ac congue sed, imperdiet sed enim. Quisque
-                luctus eget ex ut commodo. Nullam non velit sodales.
-            </p>
-            <button type="submit">About us</button>
+            <div className="about">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris elit mi, fringilla ac congue sed, imperdiet sed enim.
+                    Quisque luctus eget ex ut commodo. Nullam non velit sodales.
+                </p>
+                <button type="submit">About us</button>
+            </div>
         </header>
     );
 }
