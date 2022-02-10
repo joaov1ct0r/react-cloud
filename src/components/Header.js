e03ab9d32b5bf1ef57da453463f190cb464b5a8f
Joao Victor <jov9051@outlook.com>
2022-02-10 19:52:56 -0300

    elemento div h2-container criado em heade.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 function Header() {
     return (
         <header>
+            <div className="h2-container"></div>
             <h2>Cloud</h2>
             <h2>Computing</h2>
+
+            <div className="img-container">
+                <img
+                    src={require('../assets/images/cloud.jpg')}
+                    alt="cloud system"
+                />
+            </div>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
                 elit mi, fringilla ac congue sed, imperdiet sed enim. Quisque
                 luctus eget ex ut commodo. Nullam non velit sodales.
             </p>
             <button type="submit">About us</button>
-
-            <div className="img-container">
-                <img
-                    src={require('./assets/images/cloud.jpg')}
-                    alt="cloud system"
-                />
-            </div>
         </header>
     );
 }
