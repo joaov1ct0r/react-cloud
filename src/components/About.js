fee3a28ca8f79a0781d0fca0f1f3cfeefc7442fc
Joao Victor <jov9051@outlook.com>
2022-02-10 22:37:20 -0300

    codigo about inserido em componente about

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,14 @@
-function About() {}
+function About() {
+    return (
+        <div className="about">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                elit mi, fringilla ac congue sed, imperdiet sed enim. Quisque
+                luctus eget ex ut commodo. Nullam non velit sodales.
+            </p>
+            <button type="submit">About us</button>
+        </div>
+    );
+}
 
 export default About;
