87a1963e1c07ebb226b6d54ee93ce05b4ada65c7
Joao Victor <jov9051@outlook.com>
2022-02-10 22:34:38 -0300

    codigo h2container removido de header.js e inserido em h2container.js

--- src/components/H2Container.js
+++ src/components/H2Container.js
@@ -1,3 +1,10 @@
-function H2Container() {}
+function H2Container() {
+    return (
+        <div className="h2-container">
+            <h2>Cloud</h2>
+            <h2>Computing</h2>
+        </div>
+    );
+}
 
 export default H2Container;
